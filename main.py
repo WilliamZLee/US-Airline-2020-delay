5a110008b3bb41b5ca998bb49c2ad8be83ff0a2c
William.Zhou.Lee <duke_not_duck@outlook.com>
2023-10-17 12:28:56 +0800

    change in main

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ import matplotlib.pyplot as plt
 airports_data = pd.read_csv('D:/Duke/UWA/2023S2/5101/Team assignment/airports.csv')
 airline_delay_data = pd.read_csv('D:/Duke/UWA/2023S2/5101/Team assignment/airline_delay_causes_Feb2020.csv')
 
+'''
 airline_delay_data.info()
-airports_data.info()
+airports_data.info()'''
+
+delay_count = airline_delay_data[['arr_del15', 'carrier_ct', 'weather_ct',
+                                  'nas_ct','security_ct', 'late_aircraft_ct']]
+
+delay_time = airline_delay_data[['arr_delay', 'carrier_delay', 'weather_delay',
+                                 'nas_delay', 'security_delay', 'late_aircraft_delay']]
+
+flights_count = airline_delay_data[['arr_flights', 'arr_del15', 'arr_cancelled', 'arr_diverted']]
