177affccfc00e87b107bc76324e1481293ae24ac
William.Zhou.Lee <duke_not_duck@outlook.com>
2023-10-18 15:56:15 +0800

    change with data

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 ## import pacakge
 
-airports_data = pd.read_csv('D:/Duke/UWA/2023S2/5101/Team assignment/airports.csv')
-airline_delay_data = pd.read_csv('D:/Duke/UWA/2023S2/5101/Team assignment/airline_delay_causes_Feb2020.csv')
+airports_data = pd.read_csv('data/airports.csv')
+airline_delay_data = pd.read_csv('data/airline_delay_causes_Feb2020.csv')
 
 ## airline_delay_data.info()
 ## airports_data.info()
