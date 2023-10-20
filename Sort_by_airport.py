f4d0d1efe058303c070d88c825c0d51a722e5890
William.Zhou.Lee <duke_not_duck@outlook.com>
2023-10-20 21:34:18 +0800

    add top airport
    new change

--- Sort_by_airport.py
+++ Sort_by_airport.py
@@ -7,6 +7,20 @@ def get_top_states(airline_delay_data):
 
 
 def filter_top_airports_by_state(airline_delay_data, top_states):
+    ## delay ratio by airport
+    delay_by_airport = airline_delay_data.groupby('airport_name')
+    delay_ratio = delay_by_airport['arr_del15'].sum() / delay_by_airport['arr_flights'].sum()
+    delay_ratio_filtered = delay_ratio[delay_by_airport['arr_flights'].sum() > 10000]
+    sorted_delay_ratio = delay_ratio_filtered.sort_values(ascending=False)
+    print(sorted_delay_ratio.head(10))
+
+    ## filitered top affected airports delay factor
+    filtered_data = airline_delay_data[airline_delay_data['airport_name'].isin(sorted_delay_ratio.head(10).index)]
+    selected_columns = ['airport_name', 'state', 'carrier_ct', 'weather_ct', 'nas_ct', 'security_ct',
+                        'late_aircraft_ct', 'arr_cancelled', 'arr_diverted']
+    filtered_data_selected = filtered_data[selected_columns]
+    print(filtered_data_selected)
+
     ##  filtered data by top states
     filtered_data = airline_delay_data.loc[airline_delay_data['state'].isin(top_states)]
 
@@ -19,7 +33,7 @@ def filter_top_airports_by_state(airline_delay_data, top_states):
     '''Get the top 2 airports for each state and output '''
     top_airports = sorted_data.groupby('state').head(1)
 
-    print("Top affected airports by state: ")
+    print("\nTop affected airports by state: ")
     print(top_airports)
 
     return top_airports
--- Sort_by_state.py
+++ Sort_by_state.py
@@ -14,6 +14,12 @@ def delay_analysis_by_state(airline_delay_data, factors):
     delay_counts_total = delay_by_state['arr_del15'].sum()
     print(delay_counts_total.sort_values(ascending=False))
 
+
+    '''Sort by delay ratio'''
+    delay_ratio = delay_by_state['arr_del15'].sum() / delay_by_state['arr_flights'].sum()
+    sorted_delay_ratio = delay_ratio.sort_values(ascending=False)
+    print(sorted_delay_ratio)
+
     '''Calculate delay factors for each state separately'''
     delay_state_factor = delay_by_state[factors].sum()
 
@@ -21,12 +27,17 @@ def delay_analysis_by_state(airline_delay_data, factors):
     delay_state_factor['max'] = delay_state_factor.max(axis=1)
     delay_state_factor['max_idx'] = delay_state_factor.idxmax(axis=1)
     pd.set_option('display.max_columns', None)
-    pd.set_option('display.expand_frame_repr',False)
+    pd.set_option('display.expand_frame_repr', False)
     print(delay_state_factor)
 
-    '''Group and calculate the number of delays sorted by airline name'''
+    '''Group and calculate the number of delays caused by carrier sorted by airline name'''
     delay_by_carrier = airline_delay_data.groupby('carrier_name')
-    delay_counts_total_c = delay_by_carrier['arr_del15'].sum()
+    delay_counts_total_c = delay_by_carrier['carrier_ct'].sum()
+    print(delay_counts_total_c.sort_values(ascending=False))
+
+    '''Group and calculate the severity of delays caused by carrier sorted by airline name'''
+    delay_by_carrier = airline_delay_data.groupby('carrier_name')
+    delay_counts_total_c = delay_by_carrier['carrier_delay'].sum()
     print(delay_counts_total_c.sort_values(ascending=False))
 
     '''generate bar chart of these 7 factors'''
