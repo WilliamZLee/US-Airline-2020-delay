7459e1483b1e1bc89ccfdd96fa1e48ff9b800d4e
William.Zhou.Lee <duke_not_duck@outlook.com>
2023-10-20 12:25:14 +0800

    ADD sort by airports

---
+++ Sort_by_airport.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def get_top_states(airline_delay_data):
+    top_states = airline_delay_data.groupby('state')['late_aircraft_ct'].sum().nlargest(7).index
+    return top_states
+
+
+def filter_top_airports_by_state(airline_delay_data, top_states):
+    '''Filter the data for the top states'''
+    filtered_data = airline_delay_data.loc[airline_delay_data['state'].isin(top_states)]
+
+    '''Group by state and airport, and calculate the sum of late_aircraft_ct'''
+    grouped_data = filtered_data.groupby(['state', 'airport_name'])['late_aircraft_ct'].sum().reset_index()
+
+    '''Sort the data by state and late_aircraft_ct in descending order'''
+    sorted_data = grouped_data.sort_values(['state', 'late_aircraft_ct'], ascending=[True, False])
+
+    '''Get the top 2 airports for each state'''
+    top_airports = sorted_data.groupby('state').head(1)
+
+    print("Top affected airports by state: ")
+    print(top_airports)
+
+    return top_airports
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import importlib
 import Relation_between_Geographical_factors_delay as rbgfd
 import Filtered_airports_heatmap_plot as fahp
 import Pie_chart_plot as pcp
+import Sort_by_airport
 
 airports_data = pd.read_csv('data/airports.csv')
 airline_delay_data = pd.read_csv('data/airline_delay_causes_Feb2020.csv')
@@ -22,6 +23,8 @@ flights_count = airline_delay_data[['arr_flights', 'arr_del15', 'arr_cancelled',
 '''set index by state'''
 delay_count_row = delay_count.set_index('state', drop=True)
 delay_time_row = delay_time.set_index('state', drop=True)
+
+
 ## delay_count_row.info()
 
 def run_analysis(airline_delay_data, delay_count, airports_data, flights_count):
@@ -30,6 +33,11 @@ def run_analysis(airline_delay_data, delay_count, airports_data, flights_count):
     factors = delay_count.columns.values.tolist()
     factors.remove('state')
     Sort_by_state.delay_analysis_by_state(airline_delay_data, factors)
+
+    print("\nSort by Airports: ")
+    top_states = Sort_by_airport.get_top_states(airline_delay_data)
+    top_states = list(top_states)[:7]
+    Sort_by_airport.filter_top_airports_by_state(airline_delay_data, top_states)
 
     print("\nRelation between Geographical factors delay: ")
     rbgfd.calculate_delay_correlation(delay_count, airline_delay_data)
